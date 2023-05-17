5e8907f2c4c1bd076c9f8e05d42c0d132dcbc90a
Vanderclin <vanderclinrocha@gmail.com>
2023-05-17 10:00:12 -0300

    New update

--- update/src/main/java/com/mobiletv/update/Constants.java
+++ update/src/main/java/com/mobiletv/update/Constants.java
@@ -1,16 +1,18 @@
 package com.mobiletv.update;
 
 class Constants {
-
     static final String APK_DOWNLOAD_URL = "url";
     static final String APK_UPDATE_CONTENT = "updateMessage";
     static final String APK_VERSION_CODE = "versionCode";
-
     static final int TYPE_NOTIFICATION = 2;
-
     static final int TYPE_DIALOG = 1;
-
     static final String TAG = "UpdateChecker";
-
     static final String UPDATE_URL = "https://app.pixreward.ml/assets/update/MobileTV/update.json";
+    // Variantes adicionais
+    static final String ARTIFACT_TYPE = "artifactType";
+    static final String APPLICATION_ID = "applicationId";
+    static final String VARIANT_NAME = "variantName";
+    static final String ELEMENT_TYPE = "elementType";
+    static final String OUTPUT_FILE = "outputFile";
+
 }
